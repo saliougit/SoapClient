0fecff4a6dacd5486bacf98012383cc1885da8a5
saliougit <username@users.noreply.github.com>
2024-07-12 15:21:49 +0000

    fisrt commit

---
+++ src/main/java/EuxlouCode/ClientJavaSoap/ClientJavaSoapApplication.java
@@ -0,0 +1,13 @@
+package EuxlouCode.ClientJavaSoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientJavaSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientJavaSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EuxlouCode/ClientJavaSoap/ClientJavaSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package EuxlouCode.ClientJavaSoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientJavaSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
